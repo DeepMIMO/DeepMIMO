79bf77d4d6b3130a12f05591e43f78d2efea2396
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2025-07-23 13:06:26 -0400

    add all sequences statistics

--- testing.py
+++ testing.py
@@ -639,22 +639,17 @@ params2 = interpolate_path(dataset, idx_1, idx_2, distances)
 
 matrices = ['rx_pos', 'tx_pos', 'aoa_az', 'aod_az', 'aoa_el', 'aod_el', 
             'delay', 'power', 'phase', 'inter']
-d_10cm = dm.load('asu_campus_3p5_10cm', matrices=matrices)
-
-#%%
-
-# Generate all pairs of points 
+matrices = ['rx_pos', 'inter']
+dataset = dm.load('asu_campus_3p5_10cm', matrices=matrices)
 
 # For example: ...
-dataset = dm.load('asu_campus_3p5')
+# dataset = dm.load('asu_campus_3p5_10cm')
 # dataset = dataset.subset(dataset.get_uniform_idxs([2,2]))
 
-n_cols, n_rows = dataset.grid_size
+#%% Generate all linear sequences in a scenario
 
-folder = 'sweeps'
-os.makedirs(folder, exist_ok=True)
-
-def get_consecutive_active_segments(dataset, idxs):
+def get_consecutive_active_segments(dataset: dm.Dataset, idxs: np.ndarray,
+                                    min_len: int = 1) -> list[np.ndarray]:
     """Get consecutive segments of active users.
     
     Args:
@@ -671,30 +666,36 @@ def get_consecutive_active_segments(dataset, idxs):
     consecutive_arrays = np.split(active_idxs, splits)
     
     # Filter out single-element arrays
-    consecutive_arrays = [arr for arr in consecutive_arrays if len(arr) > 1]
+    consecutive_arrays = [arr for arr in consecutive_arrays if len(arr) > min_len]
     
     return consecutive_arrays
     
-row_or_col = 'row'
-for k in range(n_rows if row_or_col == 'row' else n_cols):
-    func = dataset.get_row_idxs if row_or_col == 'row' else dataset.get_col_idxs
-    idxs = func(k)
-    consecutive_arrays = get_consecutive_active_segments(dataset, idxs)
-    
-    print(f"{row_or_col} {k} has {len(consecutive_arrays)} consecutive segments:")
-    dataset.los.plot()
-    for i, arr in enumerate(consecutive_arrays):
-        print(f"Segment {i}: {len(arr)} users")
-        idxs_filtered = idxs[arr]
-        plt.scatter(dataset.rx_pos[idxs_filtered, 0], 
-                    dataset.rx_pos[idxs_filtered, 1], color='red', s=.5)
-    
-    plt.savefig(f'{folder}/asu_campus_3p5_{row_or_col}_{k:04d}.png', 
-                bbox_inches='tight', dpi=200)
-    plt.close()
-    # break
+#%% Make video of all sequences
 
-#%% Combine all PNGs into a video
+folder = 'sweeps'
+os.makedirs(folder, exist_ok=True)
+
+n_cols, n_rows = dataset.grid_size
+
+for row_or_col in ['row', 'col']:
+    for k in range(n_rows if row_or_col == 'row' else n_cols):
+        idx_func = dataset.get_row_idxs if row_or_col == 'row' else dataset.get_col_idxs
+        idxs = idx_func(k)
+        consecutive_arrays = get_consecutive_active_segments(dataset, idxs)
+        
+        print(f"{row_or_col} {k} has {len(consecutive_arrays)} consecutive segments:")
+        dataset.los.plot()
+        for i, arr in enumerate(consecutive_arrays):
+            print(f"Segment {i}: {len(arr)} users")
+            idxs_filtered = idxs[arr]
+            plt.scatter(dataset.rx_pos[idxs_filtered, 0], 
+                        dataset.rx_pos[idxs_filtered, 1], color='red', s=.5)
+        
+        plt.savefig(f'{folder}/asu_campus_3p5_{row_or_col}_{k:04d}.png', 
+                    bbox_inches='tight', dpi=200)
+        plt.close()
+        # break
+
 import subprocess
 
 subprocess.run([
@@ -708,13 +709,34 @@ subprocess.run([
     f"{folder}/output_60fps.mp4"
 ])
 
-#%%
+#%% Create all sequences
 
-all_seqs = []
-for k in range(n_rows):
-    idxs = dataset.get_row_idxs(k)
-    consecutive_arrays = get_consecutive_active_segments(dataset, idxs)
-    all_seqs += consecutive_arrays
+def get_all_sequences(dataset: dm.Dataset, min_len: int = 1) -> list[np.ndarray]:
+    n_cols, n_rows = dataset.grid_size
+    all_seqs = []
+    for k in range(n_rows):
+        idxs = dataset.get_row_idxs(k)
+        consecutive_arrays = get_consecutive_active_segments(dataset, idxs, min_len)
+        all_seqs += consecutive_arrays
+
+    for k in range(n_cols):
+        idxs = dataset.get_col_idxs(k)
+        consecutive_arrays = get_consecutive_active_segments(dataset, idxs, min_len)
+        all_seqs += consecutive_arrays
+
+    return all_seqs
+
+all_seqs = get_all_sequences(dataset, min_len=1)
+
+# Print statistics
+sum_len_seqs = sum([len(seq) for seq in all_seqs])
+avg_len_seqs = sum_len_seqs / len(all_seqs)
+
+print(f"Number of sequences: {len(all_seqs)}")
+print(f"Average length of sequences: {avg_len_seqs:.1f}")
+
+print(f"Number of active users: {len(dataset.get_active_idxs())}")
+print(f"Total length of sequences: {sum_len_seqs}")
 
 
 
